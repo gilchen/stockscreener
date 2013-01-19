a8fcd3b3ef3c5f9720972a4c146cd589f50e4dfe
shellverma@gmail.com <shellverma@gmail.com@c2f9c19c-2d54-11df-82ad-6129be061994>
2013-01-19 13:34:52 +0000

    Updated CallerQuote to separate call for pulling data from cnbc. Added some Utility methods. Updated TopVolumeHighlighter to generate XML on clicking a point in chart. This xml will be used by StockAlert class which is also added.

---
+++ trunk/stockscreener/src/main/java/com/stocks/standalone/Quote.java
@@ -0,0 +1,56 @@
+package com.stocks.standalone;
+
+public class Quote {
+	private String symbol;
+	private String realTime;
+	private String rangeToday;
+	private String pcChange;
+	private String time;
+	private String range52wk;
+	private String range52wkPc;
+	
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getRealTime() {
+		return realTime;
+	}
+	public void setRealTime(String realTime) {
+		this.realTime = realTime;
+	}
+	public String getRangeToday() {
+		return rangeToday;
+	}
+	public void setRangeToday(String rangeToday) {
+		this.rangeToday = rangeToday;
+	}
+	public String getPcChange() {
+		return pcChange;
+	}
+	public void setPcChange(String pcChange) {
+		this.pcChange = pcChange;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getRange52wk() {
+		return range52wk;
+	}
+	public void setRange52wk(String range52wk) {
+		this.range52wk = range52wk;
+	}
+	public String getRange52wkPc() {
+		return range52wkPc;
+	}
+	public void setRange52wkPc(String range52wkPc) {
+		this.range52wkPc = range52wkPc;
+	}
+	
+	
+}
